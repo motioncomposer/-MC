f43908b72332cfb70a05a010cde3a3541a192c7b
HitheshSreedharaFS <HitheshSreedharaFS@users.noreply.github.com>
2018-03-26 18:59:10 +0200

    #2. Second Milestone Release Version
    #2. Second Milestone Release Version

--- CM/OSC_CS.java
+++ CM/OSC_CS.java
@@ -3,14 +3,17 @@ package controlmodule.motioncomposer;
 import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PipedOutputStream;
 import java.io.PrintStream;
 //import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.SocketAddress;
 //import java.net.SocketException;
 import java.nio.file.Paths;
@@ -33,7 +36,7 @@ public class OSC_CS {
 	}
 	static List<String> csList = new ArrayList<String>();
 	static List<String> oscMessageList = new ArrayList<String>(); 
-	static String strFileName = "includes/config_.txt";
+	static String strFileName = "CM/includes/config_.txt";
 	static String strProcess="";
 	static String strOldValue;
 	static String strNewValue;
@@ -43,11 +46,17 @@ public class OSC_CS {
 	static InetAddress receiverIntAdd=null;
 	static int intSenderPort=0;
 	static int inReceiverPort = 0;
+	static InetSocketAddress CMtoTM = null;
+	static InetSocketAddress CMtoME = null;
+	static InetSocketAddress TMtoME = null;
+	static InetSocketAddress CMtoGUI= null;
+	//static InetSocketAddress MEtoTM = null;
 	static String strReceiverName = null;
 	static OSCClient senderTm;
 	static OSCClient senderMe;
 	static OSCServer receiverTm;
 	static OSCServer receiverMe;
+	static OSCServer receiverGui;
 	static String clientName=null ;
 	OSCMessage clientMsg;
 	OSCMessage serverMsg = null;
@@ -56,16 +65,24 @@ public class OSC_CS {
 	//static SimpleOSCListener listener = new SimpleOSCListener();
 	static String cmPath = Paths.get(".").toAbsolutePath().normalize().toString();
 	
+	
 	public static void osc() throws IOException, InterruptedException 
 	{
 		
     	
     	   try {
 	            // FileReader reads text files in the default encoding.
-	            FileReader fileReader = new FileReader(strFileName);
+    		   	InputStream fileReader = new FileInputStream(strFileName);
+    		    //BufferedReader buffReader = new BufferedReader((new InputStreamReader(fileReader)));
+    		    
+    		  // InputStream initialStream = new ByteArrayInputStream("With Java".getBytes());
+    		     
+    		    //Reader targetReader = new InputStreamReader(initialStream);
+    		   	
+	            //FileReader fileReaders = new FileReader(strFileName);
 
 	            // Always wrap FileReader in BufferedReader.
-	            BufferedReader bufferedReader = new BufferedReader(fileReader);
+	            BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(fileReader)));
 
 	            while((strLine = bufferedReader.readLine()) != null) {
 	            	if(strLine.startsWith("@")) {
@@ -118,11 +135,7 @@ public class OSC_CS {
 	                + strFileName + "'");                  
 	            
 	        }
-    	
-	        	
-
-    	
-	
+    	   
     	   for(String i : csList) 
 	{
     		   HashMap<String, String> csMap = new HashMap<String,String>(); 
@@ -148,16 +161,27 @@ public class OSC_CS {
 	    			String strSenderPort = csMap.get("port");
 	    			intSenderPort = Integer.parseInt(strSenderPort);
 	    			clientName=csMap.get("name");
-	    			if(clientName.equals("CMtoTM")) 
+	    			senderTm = OSCClient.newUsing(OSCClient.UDP);
+	    			
+	    			switch(clientName) 
 	    			{
-	    				senderTm = OSCClient.newUsing(OSCClient.UDP);//, new InetSocketAddress( senderIntAdd, intSenderPort));    // Create UDP Clients
-	    				//System.out.println(senderTm.getLocalAddress()+ clientName);
+	    				
+	    				case "CMtoTM":
+		    				CMtoTM  = new InetSocketAddress(senderIntAdd, intSenderPort);
+		    				break;
+	    				
+	    				case "CMtoME":
+		    				CMtoME = new InetSocketAddress(senderIntAdd, intSenderPort);
+		    				break;
+	    				
+	    				case "CMtoGUI":
+		    				CMtoGUI = new InetSocketAddress(senderIntAdd, intSenderPort);
+		    				break;
+	    				
+	    				default:
+	    					TMtoME = new InetSocketAddress(senderIntAdd, intSenderPort);
 	    			}
-	    			else
-	    			{
-	    				senderMe= OSCClient.newUsing(OSCClient.UDP); //, new InetSocketAddress( senderIntAdd, intSenderPort));    // Create UDP Clients
-	    				//System.out.println(senderMe.getLocalAddress()+ clientName);
-	    			}
+	    			 			
 	    		}
 	    		else if(typeValue.contentEquals("server"))
 	    		{
@@ -166,33 +190,38 @@ public class OSC_CS {
 	    			String strReceiverPort = csMap.get("port");
 	    			inReceiverPort = Integer.parseInt(strReceiverPort);
 	    			strReceiverName = csMap.get("name");
-	    			System.out.println(strReceiverName);
-	    			if(strReceiverName.equals("CMserverTM")) 
+	    			//System.out.println(strReceiverName);
+	    			switch(strReceiverName) 
 	    			{
-	    				receiverTm = OSCServer.newUsing(OSCServer.UDP, new InetSocketAddress( receiverIntAdd, inReceiverPort)); // Create UDP Servers
-	    				//System.out.println(receiverTm.getLocalAddress()+strReceiverName);
-	    				receiverTm.start();
-	    				
-	    			}
-	    			else
-	    			{
-	    				receiverMe = OSCServer.newUsing(OSCServer.UDP, new InetSocketAddress( receiverIntAdd, inReceiverPort)); // Create UDP Servers
-	    				//System.out.println(receiverMe.getLocalAddress()+strReceiverName);
-	    				receiverMe.start();
-	    			}
-
-	    			
+	    				case "CMserverTM":
+	    					receiverTm = OSCServer.newUsing(OSCServer.UDP, new InetSocketAddress( receiverIntAdd, inReceiverPort),strReceiverName); // Create UDP Servers
+		    				receiverTm.start();
+		    				consoleMsg(strReceiverName);
+		    				break;
+		    				
+	    				case "CMserverGUI":
+	    					receiverGui = OSCServer.newUsing(OSCServer.UDP, new InetSocketAddress( receiverIntAdd, inReceiverPort),strReceiverName);
+	    					receiverGui.start();
+	    					break;
+	    					
+	    				default:
+	    					receiverMe = OSCServer.newUsing(OSCServer.UDP, new InetSocketAddress( receiverIntAdd, inReceiverPort),strReceiverName); // Create UDP Servers
+		    				receiverMe.start();
+	    					
+	    			}    			
 	    		}
 	    		
 	     	}
-
-    	   receiverTm.addOSCListener(new OSCListener() 
+    	   
+    	 //
+    	   
+    	 receiverTm.addOSCListener(new OSCListener() 
     	   {
     	        public void messageReceived( OSCMessage m, SocketAddress addr, long time )
     	        	{
     	        		String receivedAddress = m.getName();
-    	        		String strNa =m.getName();
-    	        		consoleMsg(strNa);
+    	        		//String strNa =m.getName();
+    	        		//consoleMsg(strNa);
     	        		String strAd = addr.toString();
     	        		consoleMsg(strAd);
     	        		
@@ -209,17 +238,40 @@ public class OSC_CS {
 						}
     	        	}
     	   });
-    	  
+    	 
     	   
- 
+    	 receiverGui.addOSCListener(new OSCListener() 
+    	 {
+    		 public void messageReceived( OSCMessage m, SocketAddress addr, long time )
+	        	{
+    			 String receivedAddress = m.getName();
+ 	        		//consoleMsg(receivedAddress);
+	        		String strrAd= addr.toString();
+	        		//consoleMsg(strrAd);
+	        		
+	        		try {
+						msghandler(receivedAddress);
+					} 
+	        		catch (IOException e)
+	        		{
+	        			e.printStackTrace();
+					} 
+	        		catch (InterruptedException e) 
+	        		{
+						e.printStackTrace();
+					}
+    	 }
+    	 });
+    	 
+    	 
     	   receiverMe.addOSCListener(new OSCListener() 
     	   {
     	        public void messageReceived( OSCMessage m, SocketAddress addr, long time )
     	        	{
     	        	String receivedAddress = m.getName();
-    	        	consoleMsg(receivedAddress);
+    	        	//consoleMsg(receivedAddress);
 	        		String strrAd= addr.toString();
-	        		consoleMsg(strrAd);
+	        		//consoleMsg(strrAd);
 	        		
 	        		try {
 						msghandler(receivedAddress);
@@ -235,124 +287,166 @@ public class OSC_CS {
     	        	
     	        	}
     	   });
-  
-    	   
-    	   	
+ 	   	
 	}
 
-	
 	public static void run_osc() throws IOException, InterruptedException 
-	{
-	
-		  	  File fileTm = new File( cmPath+"\\TM\\release\\app\\main");
-   	   		  Desktop desktopTm = Desktop.getDesktop();
-   	   		  if(fileTm.exists()) desktopTm.open(fileTm);
-   	   		  
-   	   		  Thread.sleep(10000);	
-   	   		  
-   	   		  File fileMe = new File( cmPath+"\\MEs\\Tonality\\MEsimulator.maxpat");
-	   		  Desktop desktopMe = Desktop.getDesktop();
-	   		  if(fileMe.exists()) desktopMe.open(fileMe);
-   	   		  
+	{	
+				 
+		 File fileMe = new File( cmPath+"\\TM\\release\\bin\\TrackingModule.exe");
+	 	  Desktop desktopMe = Desktop.getDesktop();
+	 	  if(fileMe.exists()) desktopMe.open(fileMe);
+			   	   		 		  
 	}
+	
+	public static void startME() throws IOException, InterruptedException
+	{
+			 	  File fileMe = new File( cmPath+"\\MEs\\Tonality\\180302_Tonality_test\\ME_tester.pd");
+			 	  Desktop desktopMe = Desktop.getDesktop();
+			 	  if(fileMe.exists()) desktopMe.open(fileMe);
+	}
+	
 
 	public static void msghandler(String address) throws IOException, InterruptedException 
 	{
-		System.out.println(address);
+		//System.out.println(address);
 		
-		if(address.equals("/set/ME/loaded"))
+		if(address.equals("/set/loaded"))	
 		
 		{
-			
-			 senderMe.setTarget(new InetSocketAddress(receiverIntAdd,6560));
-			 //System.out.println(senderMe.getLocalAddress());
-			 senderMe.start();
-			 senderMe.send(new OSCMessage( "/set/ME/initialize"));
-			 String meInt="/set/ME/initialize";
-			 //System.out.println("msg sent");
-			 consoleMsg(meInt+"\n");
-			 
+				 senderTm.setTarget(CMtoME);
+				 senderTm.start();
+				 senderTm.send(new OSCMessage( "/set/initialize"));
+				 consoleMsg("Msg Received:"+address);
+				 String meInt="/set/initialize";
+				 consoleMsg("Msg Sent:"+meInt+"\n");
+				 senderTm.setTarget(CMtoGUI);
+				 senderTm.start();
+			     senderTm.send( new OSCMessage( "/set/loaded"));
 		}                                     
+		
+		else if(address.equals("start")) 
+		{
+			//consoleMsg(address+"\n");
+			startME();
+		}
+		
 		
 		else if (address.equals("/set/ME/ready")) 
 		
 		{
-			
+			 consoleMsg("Msg Received:"+address);
+			 senderTm.setTarget(CMtoTM);
+			 senderTm.start();
+			 senderTm.send(new OSCMessage( "/set/player/0/tracking ,i 1"));
+			 consoleMsg("Msg Sent:"+"/set/player/0/tracking ,i 1 to TM");
+		}
+		
+		else if  ((address.equals("/set/player/1/soundbank/1/list"))|| (address.equals("/set/player/1/soundbank/2/list"))||
+				  (address.equals("/set/player/2/soundbank/1/list"))||(address.equals("/set/player/2/soundbank/2/list"))) 
+		
+		{
+	
+				consoleMsg(address+"\n");
+				
+		
+		}
+		
+		else if(address.equals("/set/error"))
+		{
 			
 		}
 		
-		else if  ((address.equals("/set/player/1/soundbank/1/list") )|| (address.equals("/set/player/1/soundbank/2/list"))||(address.equals("/set/player/2/soundbank/1/list")) ||(address.equals("/set/player/2/soundbank/2/list"))) 
-		
+		else if (address.equals("/set/zone/1/soundbank/1/list")||address.equals("/set/zone/2/soundbank/1/list")) 
 		{
-			
-			
+				consoleMsg(address+"\n");
 		}
 		
 		else if (address =="/set/xxx") 
 		
 		{
 			
-			
 		}
 		
 		else if (address == "/set/xxx") 
 		
 		{
-			
-			
-		}
-			
 		
+		}
+
 		else if(address.equals("/set/TM/loaded")) 
 		
 		{
-			 
-			 
-			 senderTm.setTarget(new InetSocketAddress( "127.0.0.1", 6561));
-			 senderTm.start();
-			 senderTm.send( new OSCMessage( "/set/TM/initialize"));
-			 String strTm = "/set/TM/initialize";
-			 consoleMsg(strTm+"\n");
+				 //senderTm.setTarget(CMtoTM);
+				 //senderTm.start();
+				 //senderTm.send( new OSCMessage( "/set/TM/initialize"));
+				 //String strTm = "/set/TM/initialize";
+				 consoleMsg("Msg Received:"+address);
+				 //consoleMsg("Msg Sent:"+strTm+"\n");
 			 //System.out.println("msg sent");
 		}
 	
 		else if(address.equals("/set/TM/ready")) 
 		
-		{
-			
-			String nwMsg = "msg received";
-			consoleMsg(nwMsg+"\n");
-			
+		{	
+				Socket tcpClient = new Socket("localhost", 50000);
+				String nwMsg = "msg received";
+				consoleMsg("Msg Received:"+address);
+				consoleMsg(nwMsg+"\n");
+				senderTm.setTarget(CMtoGUI);
+				senderTm.start();
+			    senderTm.send( new OSCMessage( "TM is Ready"));
+			    //senderTm.setTarget(CMtoTM);
+			    // senderTm.start();
+			    //senderTm.send(new OSCMessage("/set/player/0/tracking ,i 1"));
 		}
 		
+		else if(address.equals("/set/player/0/tracking ,i 0"))
+		{
+			consoleMsg("Msg Received:"+address);
+		}
+		else 
+		{
+			consoleMsg("Msg Received:"+address);
+		}
+		
+
 	}
 	
 	public static void display() 
-	{
-		ImageIcon img = new ImageIcon("includes/logo1.gif");
+			
+		{
 		
-		  //JFrame frame = new JFrame("MotionComposer");
-		 
-		  	 frame.setSize(500,200);
-		     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		     frame.setIconImage(img.getImage());
-		  
-		  
-		  	//JTextArea textField = new JTextArea();
-	        textField.setBounds(150, 150, 280, 150);
-	        textField.setVisible(true);
-	        frame.add(textField);
-	        frame.setVisible(true);
-	        
-	        PipedOutputStream pOut = new PipedOutputStream();
-	        System.setOut(new PrintStream(pOut,true));
-	        
-	        
-	}
+			ImageIcon img = new ImageIcon("CM/includes/logo1.gif");
+			frame.setSize(500,200);
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    frame.setIconImage(img.getImage());
+	
+		    textField.setBounds(150, 150, 280, 150);
+		    textField.setVisible(true);
+		    frame.add(textField);
+		    frame.setVisible(true);
+		        
+		    PipedOutputStream pOut = new PipedOutputStream();
+		    System.setOut(new PrintStream(pOut,true));
+      
+		}
 	
 	public static void consoleMsg(String msgCon) 
-	{
-		textField.append(msgCon+"\n");
-	}
+	
+		{
+		
+			textField.append(msgCon+"\n");
+		
+		}
+	
+	
+	public static void send_osc()
+		
+		{
+		
+		
+		//senderTm.send(clientMsg);
+		}
 	
 }
